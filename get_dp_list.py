092a51cbd2add90ca4a9be1e164e1d93ee111c8c
Jules <julesfarjas@gmail.com>
2019-11-17 13:06:11 -0300

    First commit

---
+++ get_dp_list.py
@@ -0,0 +1,31 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+import csv
+import os
+
+LIST_URL = "https://www.camara.leg.br/internet/agencia/infograficos-html5/DeputadosEleitos/index.html"
+
+
+def scrape():
+    with open(os.path.join('data', 'employee_file.csv'), mode='w') as output_file:
+        # Initalizing the csv output
+        writer = csv.writer(output_file)
+        writer.writerow(['name','party'])
+        response = requests.get(LIST_URL)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for line in soup.findAll('tr', attrs={'class': None}):
+            line_soup = BeautifulSoup(str(line), 'html.parser')
+            deputado = []
+            for element in line_soup.findAll('td', attrs={'class': None}):
+                deputado.append(element.get_text())
+            if deputado:
+                writer.writerow(deputado)
+                print(deputado)
+
+def main():
+    scrape()
+
+if __name__ == '__main__':
+    main()
