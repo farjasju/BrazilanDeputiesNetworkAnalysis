58ebc32d5d73a00c578277f7fe99b9c6f80ef82b
Jules <julesfarjas@gmail.com>
2019-12-08 00:17:47 -0300

    Adding account id

--- get_twitter_accounts.py
+++ get_twitter_accounts.py
@@ -4,6 +4,7 @@ import os
 import twitter
 from config import API_KEY, API_SECRET, ACCESS_TOKEN, TOKEN_SECRET
 
+
 def fetch_accounts(api):
     accounts_fetched = 0
     with open(os.path.join('data', 'accounts_raw_list.txt'), mode='w') as raw_list:
@@ -11,23 +12,27 @@ def fetch_accounts(api):
             reader = csv.reader(list_file)
             with open(os.path.join('data', 'deputies_accounts.csv'), mode='w') as output_file:
                 writer = csv.writer(output_file)
-                writer.writerow(['name','party','account'])
+                writer.writerow(['name', 'party', 'screen_name', 'id'])
                 for line in list_file:
                     name, party = line.strip('\n').split(',')
                     for user in api.GetUsersSearch(name):
                         if "deputad" in user.description.lower():
                             accounts_fetched += 1
-                            raw_list.write('"@' + user.screen_name + '",' + '\n')
-                            writer.writerow([name, party, user.screen_name])
+                            raw_list.write(
+                                '"@' + user.screen_name + '",' + '\n')
+                            writer.writerow(
+                                [name, party, user.screen_name, user.id_str])
                             print("Number of accounts found:", accounts_fetched)
                     time.sleep(1)
+
 
 def main(api):
     fetch_accounts(api)
 
+
 if __name__ == '__main__':
     api = twitter.Api(consumer_key=API_KEY,
-                  consumer_secret=API_SECRET,
-                  access_token_key=ACCESS_TOKEN,
-                  access_token_secret=TOKEN_SECRET)
+                      consumer_secret=API_SECRET,
+                      access_token_key=ACCESS_TOKEN,
+                      access_token_secret=TOKEN_SECRET)
     main(api=api)
