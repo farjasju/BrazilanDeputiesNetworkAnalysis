119fad96a62a15966116e86287cc9c15ea9f0dfd
Jules <julesfarjas@gmail.com>
2019-12-09 20:21:16 -0300

    Adding stopwords and unuseful words

--- custom_stopwords.py
+++ custom_stopwords.py
@@ -1,1 +1,9 @@
-custom_stopwords = ['``', "''", "…", "...", "rt", '”', 'p/', '“', 'r']
+custom_stopwords = ['``', "''", "…", "...",
+                    "rt", '”', 'p/', '“', 'r', '–', 'c/', 'q', 'i', 'a…', 'd', 'p', 'do…']
+useless_words = ['sobre']
+# pl = projeto de lei
+# dep = deputado
+# mp = ministerio publico
+# pr ?
+# gt = gente ?
+# rs ?
